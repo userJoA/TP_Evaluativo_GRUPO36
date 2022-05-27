852a518de2281972305aa8532ceb92f3364db27b
user <user@DESKTOP-26DH5CC>
2022-05-26 23:46:00 -0300

    creacion de proyecto

---
+++ TPMusic/src/main/java/ar/edu/unju/fi/TpMusicApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpMusicApplication.class, args);
+	}
+
+}

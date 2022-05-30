29163501b79a6bbea321b39d753bf03408a1d875
jona <jevkjev@gmail.com>
2022-05-30 14:02:14 -0300

    funcion guardar,modificar y eliminar usuario

---
+++ TPMusic/src/main/java/ar/edu/unju/fi/service/imp/UsuarioServiceImp.java
@@ -0,0 +1,113 @@
+package ar.edu.unju.fi.service.imp;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.Model.Usuario;
+import ar.edu.unju.fi.Util.lista_usuarios;
+import ar.edu.unju.fi.service.IUsuarioService;
+@Service("usuarioServiceList")
+public class UsuarioServiceImp implements IUsuarioService {
+ 
+	@Autowired
+	private lista_usuarios listaUsuarios;
+	
+
+	
+	@Override
+	public Usuario buscarUsuario(int numero) {
+		 Optional <Usuario> usuario= listaUsuarios.getUsuarios().stream().filter(a->a.getNumero() == numero).findFirst();
+		 return usuario.get();
+		
+	}
+
+
+
+	@Override
+	public Usuario getUsuario() {
+		
+		return new Usuario();
+	}
+
+
+
+	@Override
+	public lista_usuarios listaUsuario() {
+		
+		return listaUsuarios;
+	}
+
+
+
+	@Override
+	public boolean guardarUsuario(Usuario usuario) {
+		
+		return listaUsuarios.getUsuarios().add(usuario);
+	}
+
+
+
+	@Override
+	public void modificarUsuario(Usuario usuario) {
+		for(Usuario c:listaUsuarios.getUsuarios()) {
+			if(c.getNumero()==usuario.getNumero()) {
+				c.setNombre(usuario.getNombre());
+				c.setApellido(usuario.getApellido());
+				c.setFecha(usuario.getFecha());
+				
+			}
+		}
+		
+	}
+
+
+
+	@Override
+	public void eliminarUsuario(int numero) {
+		
+		for(Usuario c: listaUsuarios.getUsuarios()) {
+			if(c.getNumero()==numero) {
+				listaUsuarios.getUsuarios().remove(c);
+				break;
+			}
+			
+		}
+		
+		
+
+	}
+
+	@Override
+	public int sumaVotos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	public int sacarEdad() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	
+
+	
+
+
+
+	
+
+
+
+	
+
+
+	
+
+}

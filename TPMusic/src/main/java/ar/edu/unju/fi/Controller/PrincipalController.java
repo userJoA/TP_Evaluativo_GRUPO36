4db665f136ed40c6a9dc3046b00dd1e4b6d7cc80
jona <jevkjev@gmail.com>
2022-06-02 20:34:00 -0300

    agregando controller

--- TPMusic/src/main/java/ar/edu/unju/fi/Controller/PrincipalController.java
+++ TPMusic/src/main/java/ar/edu/unju/fi/Controller/PrincipalController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,6 +18,7 @@ import ar.edu.unju.fi.Util.lista_candidatos;
 import ar.edu.unju.fi.service.ICandidatoService;
 
 @Controller
+//@RequestMapping("/principal")
 public class PrincipalController {
 	
 	@Autowired

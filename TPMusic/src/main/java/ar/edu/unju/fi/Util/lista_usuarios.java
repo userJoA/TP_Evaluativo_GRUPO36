6d925b5f866180a6807f08466cdfdb27dc3ecc51
arielg <arielg@192.168.0.15>
2022-06-03 23:41:45 -0300

    Getters de Edad y habilitado de Usuario

--- TPMusic/src/main/java/ar/edu/unju/fi/Util/lista_usuarios.java
+++ TPMusic/src/main/java/ar/edu/unju/fi/Util/lista_usuarios.java
@@ -6,35 +6,26 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Component;
 
 import ar.edu.unju.fi.Model.Usuario;
+
 @Component
 public class lista_usuarios {
-	private ArrayList<Usuario> Usuarios;
+	
+	private ArrayList<Usuario> usuarios;
 
-	public lista_usuarios() {
-		
-		  Usuarios=new ArrayList<>(); 
-			
-			  Usuario usuario1= new Usuario(1,"Jonatan", "Vera", "jana@mail.com",LocalDate.of(2,04,8), 28, 3, true); 
-			  Usuario usuario2= new Usuario(2,"jona","V", "j@mail.com", LocalDate.of(1,02,9), 21, 2, false);
-			  
-			  
-			  Usuarios.add(usuario1); Usuarios.add(usuario2);
-			  
-			 
-		 
+	public lista_usuarios() {		
+		  usuarios=new ArrayList<>();			
+		  Usuario usuario1= new Usuario(1,"Jonatan", "Vera", "jana@mail.com",LocalDate.of(2,04,8)); 
+		  Usuario usuario2= new Usuario(2,"jona","V", "j@mail.com", LocalDate.of(1,02,9));		  
+		  usuarios.add(usuario1);
+		  usuarios.add(usuario2);		 
 	}
 
 	public ArrayList<Usuario> getUsuarios() {
-		return Usuarios;
+		return usuarios;
 	}
 
-	
 	public void setUsuarios(ArrayList<Usuario> usuarios) {
-		Usuarios = usuarios;
-	}
-	
-	
-	
-	
+		this.usuarios = usuarios;
+	}	
 	
 }
